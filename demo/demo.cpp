0806f36484106ac1f986cd73f90c494b2e30fbe8
Liu Yu Bao <liu@aisl.cs.tut.ac.jp>
2020-01-13 17:19:12 +0900

    Generate txt format vocabulary which is compatible with ORB SLAM2

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -17,28 +17,27 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/features2d.hpp>
 
-
 using namespace DBoW2;
 using namespace std;
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-void loadFeatures(vector<vector<cv::Mat > > &features);
+void loadFeatures(vector<vector<cv::Mat>> &features);
 void changeStructure(const cv::Mat &plain, vector<cv::Mat> &out);
-void testVocCreation(const vector<vector<cv::Mat > > &features);
-void testDatabase(const vector<vector<cv::Mat > > &features);
+void testVocCreation(const vector<vector<cv::Mat>> &features);
+void testDatabase(const vector<vector<cv::Mat>> &features);
 
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 // number of training images
 const int NIMAGES = 4;
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 void wait()
 {
-  cout << endl << "Press enter to continue" << endl;
+  cout << endl
+       << "Press enter to continue" << endl;
   getchar();
 }
 
@@ -46,7 +45,7 @@ void wait()
 
 int main()
 {
-  vector<vector<cv::Mat > > features;
+  vector<vector<cv::Mat>> features;
   loadFeatures(features);
 
   testVocCreation(features);
@@ -60,7 +59,7 @@ int main()
 
 // ----------------------------------------------------------------------------
 
-void loadFeatures(vector<vector<cv::Mat > > &features)
+void loadFeatures(vector<vector<cv::Mat>> &features)
 {
   features.clear();
   features.reserve(NIMAGES);
@@ -68,7 +67,7 @@ void loadFeatures(vector<vector<cv::Mat > > &features)
   cv::Ptr<cv::ORB> orb = cv::ORB::create();
 
   cout << "Extracting ORB features..." << endl;
-  for(int i = 0; i < NIMAGES; ++i)
+  for (int i = 0; i < NIMAGES; ++i)
   {
     stringstream ss;
     ss << "images/image" << i << ".png";
@@ -80,7 +79,7 @@ void loadFeatures(vector<vector<cv::Mat > > &features)
 
     orb->detectAndCompute(image, mask, keypoints, descriptors);
 
-    features.push_back(vector<cv::Mat >());
+    features.push_back(vector<cv::Mat>());
     changeStructure(descriptors, features.back());
   }
 }
@@ -91,7 +90,7 @@ void changeStructure(const cv::Mat &plain, vector<cv::Mat> &out)
 {
   out.resize(plain.rows);
 
-  for(int i = 0; i < plain.rows; ++i)
+  for (int i = 0; i < plain.rows; ++i)
   {
     out[i] = plain.row(i);
   }
@@ -99,11 +98,12 @@ void changeStructure(const cv::Mat &plain, vector<cv::Mat> &out)
 
 // ----------------------------------------------------------------------------
 
-void testVocCreation(const vector<vector<cv::Mat > > &features)
+void testVocCreation(const vector<vector<cv::Mat>> &features)
 {
-  // branching factor and depth levels 
-  const int k = 9;
-  const int L = 3;
+  // branching factor and depth levels
+  const int k = 10;
+  const int L = 6;
+
   const WeightingType weight = TF_IDF;
   const ScoringType score = L1_NORM;
 
@@ -114,63 +114,67 @@ void testVocCreation(const vector<vector<cv::Mat > > &features)
   cout << "... done!" << endl;
 
   cout << "Vocabulary information: " << endl
-  << voc << endl << endl;
+       << voc << endl
+       << endl;
 
   // lets do something with this vocabulary
   cout << "Matching images against themselves (0 low, 1 high): " << endl;
   BowVector v1, v2;
-  for(int i = 0; i < NIMAGES; i++)
+  for (int i = 0; i < NIMAGES; i++)
   {
     voc.transform(features[i], v1);
-    for(int j = 0; j < NIMAGES; j++)
+    for (int j = 0; j < NIMAGES; j++)
     {
       voc.transform(features[j], v2);
-      
+
       double score = voc.score(v1, v2);
       cout << "Image " << i << " vs Image " << j << ": " << score << endl;
     }
   }
 
   // save the vocabulary to disk
-  cout << endl << "Saving vocabulary..." << endl;
-  voc.save("small_voc.yml.gz");
+  cout << endl
+       << "Saving vocabulary..." << endl;
+  // voc.save("small_voc.yml.gz");
+  voc.saveToTextFile("demo.txt");
   cout << "Done" << endl;
 }
 
 // ----------------------------------------------------------------------------
 
-void testDatabase(const vector<vector<cv::Mat > > &features)
+void testDatabase(const vector<vector<cv::Mat>> &features)
 {
   cout << "Creating a small database..." << endl;
 
   // load the vocabulary from disk
   OrbVocabulary voc("small_voc.yml.gz");
-  
+
   OrbDatabase db(voc, false, 0); // false = do not use direct index
   // (so ignore the last param)
-  // The direct index is useful if we want to retrieve the features that 
+  // The direct index is useful if we want to retrieve the features that
   // belong to some vocabulary node.
   // db creates a copy of the vocabulary, we may get rid of "voc" now
 
   // add images to the database
-  for(int i = 0; i < NIMAGES; i++)
+  for (int i = 0; i < NIMAGES; i++)
   {
     db.add(features[i]);
   }
 
   cout << "... done!" << endl;
 
-  cout << "Database information: " << endl << db << endl;
+  cout << "Database information: " << endl
+       << db << endl;
 
   // and query the database
   cout << "Querying the database: " << endl;
 
   QueryResults ret;
-  for(int i = 0; i < NIMAGES; i++)
+  for (int i = 0; i < NIMAGES; i++)
   {
     db.query(features[i], ret, 4);
 
-    // ret[0] is always the same image in this case, because we added it to the 
+    // ret[0] is always the same image in this case, because we added it to the
     // database. ret[1] is the second best match.
 
     cout << "Searching for Image " << i << ". " << ret << endl;
@@ -183,13 +187,12 @@ void testDatabase(const vector<vector<cv::Mat > > &features)
   cout << "Saving database..." << endl;
   db.save("small_db.yml.gz");
   cout << "... done!" << endl;
-  
-  // once saved, we can load it again  
+
+  // once saved, we can load it again
   cout << "Retrieving database once again..." << endl;
   OrbDatabase db2("small_db.yml.gz");
-  cout << "... done! This is: " << endl << db2 << endl;
+  cout << "... done! This is: " << endl
+       << db2 << endl;
 }
 
 // ----------------------------------------------------------------------------
-
-
